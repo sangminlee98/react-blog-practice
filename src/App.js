a334ead05d4b7a2fdf893fce107ca8f7fec20c94
이상민 <soone@isangmin-ui-MacBookPro.local>
2021-07-04 17:58:26 +0900

    Add Modal On/Off button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+/* eslint-disable */
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파주 강아지 카페']);
+  let [따봉,따봉변경] = useState(0);
+  let [modal,modal변경] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3>{글제목[0]} <span onClick={ () => {따봉변경(따봉+1)} }>👍</span> {따봉} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>5월 25일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{글제목[2]}</h3>
+        <p>7월 4일 발행</p>
+        <hr/>
+      </div>
+      <button onClick={ () => { modal변경(!modal) }  }> 버튼 </button>
+      {
+        modal === true
+        ? <Modal/>
+        : null 
+      }
     </div>
   );
 }
+
+function Modal() {
+  return(
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>세부내용</p>
+    </div>
+  )
+}
+
 
 export default App;
