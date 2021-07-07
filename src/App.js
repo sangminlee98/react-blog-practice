ddda61f01b4ef9dd38611b798a3ab0d97b380a0d
이상민 <soone@isangmin-ui-MacBookPro.local>
2021-07-07 16:01:09 +0900

    글 발행기능 추가 및 예전문법과 신문법 차이 정리

--- src/App.js
+++ src/App.js
@@ -2,51 +2,79 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
 
   let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파주 강아지 카페']);
   let [따봉,따봉변경] = useState(0);
   let [modal,modal변경] = useState(false);
-
-  return (
+  let [누른제목,누른제목변경] = useState(0);
+  let [입력값,입력값변경] = useState('');
+  
+    return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <div className="list">
-        <h3>{글제목[0]} <span onClick={ () => {따봉변경(따봉+1)} }>👍</span> {따봉} </h3>
-        <p>2월 17일 발행</p>
-        <hr/>
+      {
+        글제목.map(function(a,i){
+          return(
+            <div className="list" key={i}>
+            <h3 onClick={()=>{누른제목변경(i)}} >{a} <span onClick={ () => {따봉변경(따봉+1)} }>👍</span> {따봉}</h3>
+            <p>5월 25일 발행</p>
+            <hr/>
       </div>
-      <div className="list">
-        <h3>{글제목[1]}</h3>
-        <p>5월 25일 발행</p>
-        <hr/>
+          )
+        })
+      }
+
+      <div className="publish">
+        <input onChange={ (e) => {입력값변경(e.target.value)} }/>
+        <button onClick={ () => {
+          var newArray = [...글제목];
+          newArray.unshift(입력값);
+          글제목변경(newArray);
+        }} >저장</button>
       </div>
-      <div className="list">
-        <h3>{글제목[2]}</h3>
-        <p>7월 4일 발행</p>
-        <hr/>
-      </div>
-      <button onClick={ () => { modal변경(!modal) }  }> 버튼 </button>
+
+      <button onClick={ () => { modal변경(!modal) }  }> 열고닫기 </button>
       {
         modal === true
-        ? <Modal/>
+        ? <Modal 글제목={글제목} 누른제목={누른제목}/>
         : null 
       }
     </div>
   );
 }
 
-function Modal() {
+//신문법 component 만드는 법
+function Modal(props) {
   return(
     <div className="modal">
-      <h4>제목</h4>
+      <h4>제목 : {props.글제목[props.누른제목]}</h4>
       <p>날짜</p>
       <p>세부내용</p>
     </div>
   )
+}
+
+//옛날문법 component 만드는 법
+class Profile extends React.Component {
+  constructor(){
+    super()
+    this.state = { name1 : 'kim', age : 12}
+  }
+  changeName() {
+    this.setState( { name1 : 'Lee'});
+  }
+  render(){
+    return(
+      <>
+        <div>안녕 나는 {this.state.name1}이고 {this.state.age}살이야</div>
+        <button onClick={ ()=> {this.setState({name1 : 'Park'})} } >변경1</button>
+        <button onClick={this.changeName.bind(this)} >변경2</button>  {/*changeName함수를 ArrowFunction으로 만들면 bind(this)안해도됨*/}
+      </>
+    )
+  }
 }
 
 
